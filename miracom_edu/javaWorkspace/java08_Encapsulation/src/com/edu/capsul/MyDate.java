488e0766d1de2c3fd2dc260f6cfe0614432bb1e5
msio900 <msio900@gmail.com>
2021-10-22 11:07:11 +0900

    1022 2nd commit

--- miracom_edu/javaWorkspace/java08_Encapsulation/src/com/edu/capsul/MyDate.java
+++ miracom_edu/javaWorkspace/java08_Encapsulation/src/com/edu/capsul/MyDate.java
@@ -1,5 +1,11 @@
 package com.edu.capsul;
 //오늘 날짜 정보를 가지고 있는 클래스...
+/*
+ * Encapsulation Pattern
+ * 1. field앞에 private을 지정 -- 다른 클래스에서 필드에 값할당 못함(직접적인 접근을 막는다.)
+ * 2. void setXxxx(0,0)  / int return타입지정 getXxxx() 는 public으로 지정
+ * 3. setXxxx() method에서 필드 초기화 되기 직전에 받은 값이 타당한 값인지에 대한 유효성 검사
+ */
 public class MyDate {
 	//private은 같은 클래스에서만 ...접근 가능... 다른 class에서 접근 불가
 	private int month;
@@ -11,10 +17,12 @@ public class MyDate {
 	public void setMonth(int month) {
 		// invalid한 값이 전달되더라도 필드 초기화 되기 직전에 걸러낼 수 있다.
 		// month에 해당하는 값이 1~12일 경우에만 필드초기화 되도록한다.
+		// field에 타당한 값만 들어가도록 유효성 검사 진행
 		if(month >= 1 & month <= 12) {
-			this.month = month;
+			this.month = month;//1~12
 		}else {
 			System.out.println("잘못된 월 입력입니다.");
+			System.exit(0); //정상 종료, -1(비정상종료)
 		}
 	}
 	public int getDay() {
@@ -22,41 +30,40 @@ public class MyDate {
 	}
 	public void setDay(int day) {
 		switch(month) {
-			case 1:
-			case 3:
-			case 5:
-			case 7:
-			case 8:
-			case 10:
-			case 12:
-				if(day >= 1 & day <= 31) {
-					this.day = day;
-				}else {
-					System.out.println("잘못된 일 입력입니다.");
-				}
-			case 4:
-			case 6:
-			case 9:
-			case 11:
-				if(day >= 1 & day <= 30) {
-					this.day = day;
-				}else {
-					System.out.println("잘못된 일 입력입니다.");
-				}
 			case 2:
 				if(day >= 1 & day <= 28) {
 					this.day = day;
-				}else {
+				}else { 
 					System.out.println("잘못된 일 입력입니다.");
+					System.exit(0); //여기서 프로그램을 끝내도록...다시 Test가 돌아가지 않도록
 				}
-		}
+					break;
+				case 4:
+				case 6:
+				case 9:
+				case 11:
+					if(day >= 1 & day <= 30) {
+						this.day = day;
+					}else { 
+						System.out.println("잘못된 일 입력입니다.");
+						System.exit(0); //여기서 프로그램을 끝내도록...다시 Test가 돌아가지 않도록
+						}
+						break;
+				default:
+					if(day >= 1 & day <= 31) {
+						this.day = day;
+					}else {
+						System.out.println("잘못된 일 입력입니다.");
+						System.exit(0); //여기서 프로그램을 끝내도록...다시 Test가 돌아가지 않도록
+					}
+					break;
+				}//switch
 		/*
 		 * switch문을 사용
 		 * 1,3,5,7,8,10,12월일떄는 day가 1~31일
 		 * 4,6,9,11월일때는 day가 1~30일
 		 * 2월일때는 day가 1~28일까지의 값을 갖도록 로직을 제어하세요
 		 */
-		this.day = day;
-	}
+	}//setDay
 
 }
--- miracom_edu/javaWorkspace/java08_Encapsulation/src/com/edu/capsul/test/MyDateTest.java
+++ miracom_edu/javaWorkspace/java08_Encapsulation/src/com/edu/capsul/test/MyDateTest.java
@@ -1,8 +1,11 @@
 package com.edu.capsul.test;
+import java.util.Scanner;
+
 /*
  * Encapsulation Pattern
- * 1. 
- * 2.
+ * 1. 외부(다른 class)로부터 직접적인 접근을 막을때 
+ * `private`을 설정 - 저장은 되지만,,, 접근이 불가함.
+ * 2.field값을 set() / get() method로 소통 public으로 열어 놓음.
  * 3. set() method 어디에 제어문을 달아야 하는지.
  */
 import com.edu.capsul.MyDate;
@@ -10,16 +13,29 @@ import com.edu.capsul.MyDate;
 public class MyDateTest {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("오늘 날짜를 입력하세요.");
 		MyDate md = new MyDate(); //Ctrl+ Shift + o
+		
+		System.out.println("월 입력>>");
+		int month = sc.nextInt();
+		
+		System.out.println("날짜 입력>>");
+		int day = sc.nextInt();
+		
+		sc.close();
 		//필드에 직접적인 접근은 막아둔다.
 		/*md.month = 33;	// invalid 값이 들어감.
 		md.day = 33;	// invalid 값이 들어감.
-*/	
-		md.setMonth(33);
-		md.setDay(33);
+		 */
+		//method...call setMonth()를 먼저...(월에 따라서 날짜가 달라지기 때문)
+		md.setMonth(month);
+		md.setDay(day);
 		//출력될때 0월 21일
 		//10월 0일 ...이런 값이 최종적으로 출력이 안되도록 하시기 바랍니다.
-		
+		//1. case 0을 추가한다.
+		//2. system.exit(0)
+
 		System.out.println("오늘은 "+md.getMonth()+"월 "+md.getDay()+"일");
 		}
 
