f71aca540b203f61a3eaacab1ba92d9956d1a49b
msio900 <msio900@gmail.com>
2021-10-29 11:54:52 +0900

    1029 2nd commit

---
+++ miracom_edu/javaWorkspace/java19_CollectionAPI/src/com/edu/collection/test/PropertiesTest1.java
@@ -0,0 +1,18 @@
+package com.edu.collection.test;
+
+import java.util.Enumeration;
+import java.util.Properties;
+
+public class PropertiesTest1 {
+	public static void main(String[] args) {
+		//ies는 다 가져옴. 인자 값이 들어있는 것은 나가리
+		Properties p = System.getProperties();
+		
+		Enumeration e = p.propertyNames();
+		while(e.hasMoreElements()) {
+			String name = (String)e.nextElement();
+			String value = p.getProperty(name);
+			System.out.println(name+"=========="+value);
+		}//while
+	}//main
+}//class
