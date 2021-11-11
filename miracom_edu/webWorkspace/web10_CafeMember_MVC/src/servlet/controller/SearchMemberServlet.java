d17cc9e68f05fcfadaf753c763ba2df199bdd989
msio900 <msio900@gmail.com>
2021-11-11 15:33:18 +0900

    1111 2nd commit

--- miracom_edu/webWorkspace/web10_CafeMember_MVC/src/servlet/controller/SearchMemberServlet.java
+++ miracom_edu/webWorkspace/web10_CafeMember_MVC/src/servlet/controller/SearchMemberServlet.java
@@ -7,35 +7,45 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class SearchMemberServlet
- */
+import servlet.model.MemberDAOImpl;
+import servlet.model.MemberVO;
+
 @WebServlet("/SMS")
 public class SearchMemberServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public SearchMemberServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+   
+	protected void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 로직은 여기에 작성...
+		/*
+		 * 1. 폼값 받아서
+		 * 2. (vo 생성....)
+		 * 3. DAO 리턴받아서...Business Logic 호출...
+		 * 4. 반환값 바인딩
+		 * 5. 네비게이션...result_view.jsp
+		 */
+		
+		String path = "index.html";
+		String id = request.getParameter("id");
+		MemberDAOImpl dao = MemberDAOImpl.getInstance();
+		try {
+		MemberVO vo = dao.findByIdMember(id);
+		if(vo != null) { //id에 해당하는 Member가 있다면
+			request.setAttribute("vo", vo);
+			path = "result_view.jsp";
+		}
+		
+		}catch(Exception e) {
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+		}
+		request.getRequestDispatcher(path).forward(request, response);
+	}
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		doProcess(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
 	}
 
 }
